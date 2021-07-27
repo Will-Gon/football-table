0d60bf7e211b1afe2014fbad63dbccd779aeb212
Will-Gon <will.colocho@gmail.com>
2021-07-26 21:14:57 -0700

    add a simple table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Table from './components/Table';
 import './App.css';
+import * as ReactBootStrap from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const players = [
+    {catches: "", touchdowns: ""}
+  ]
+
+  const renderTable = (player, index) => {
+    return (
+      <tr key={index}>
+        <td>{player.catches}</td>
+        <td>{player.touchdowns}</td>
+      </tr>
+    );
+  }
   return (
     <div className="App">
-      <Table />
+      <ReactBootStrap.Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>Touchdowns</th>
+      <th>Catches</th>
+    </tr>
+  </thead>
+  <tbody>
+    {players.map(renderTable)}
+  </tbody>
+</ReactBootStrap.Table>
+      {/* <Table /> */}
     </div>
   );
-}
+};
 
 export default App;
